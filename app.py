1df23d75bf7a663ff5f98484ee7a3cc8fcaf6c7e
Joan Cortes Torandell <160049122+JoanCort04@users.noreply.github.com>
2025-01-20 21:02:19 +0100

    Començat a montar el JWT. Esquema de /llistaamics i /grups

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from pydantic import BaseModel
 from jose import JWTError, jwt  # importar pip install python-jose
 from datetime import datetime, timedelta
 from typing import Optional, List
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
 import db
 
 db = db.Connexio()
@@ -24,17 +27,28 @@ class Usuari(BaseModel):
     username: str  # Assuming the field is 'username' instead of 'name'
     password: str
 
-
 class Token(BaseModel):
     access_token: str
     token_type: str
 
 
-@app.get("/llistaamics", response_model=List[Usuari])
-def get_usuaris():
-    db.conecta()  # Connect to the database
-    usuarios = db.cargaUsuaris()  # Get users from the database
+""" per treure el json de tots els usuaris  """
+@app.get("/llistaamics", response_model=Usuari)
+def get_usuaris(username: str):
+    db.conecta()  
+    usuarios = db.cargaUsuarisProva(username) 
     return usuarios
+
+
+@app.post("/grups", response_model=Usuari)
+def autentificar(username: str, password: str):
+    user = get_usuaris(username)
+
+    
+    if user and user["password"] == password:
+        return Usuari(**user)  
+
+    return None 
 
 
 def crearToken(data: dict, expires_delta: Optional[timedelta] = None):
@@ -48,9 +62,28 @@ def crearToken(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        # Decodifica el token JWT
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="No se pudo validar las credenciales",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return username
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token no válido",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
 @app.post("/token", response_model=Token)
 async def login(user: Usuari):
-    authenticated_user = authenticate_user(user.name, user.password)
+    authenticated_user = autentificar(user.username, user.password)
     if not authenticated_user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -59,7 +92,7 @@ async def login(user: Usuari):
         )
     # Genera el token de acceso
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
+    access_token = crearToken(
         data={"sub": authenticated_user["username"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
